dc39e953216f622769bd4fdd80fdda8be8c59081
Atar <atar.pandey@proptiger.com>
2019-08-04 20:56:55 +0530

    added log

--- app/src/main/java/com/atar/fastlane/MainActivity.java
+++ app/src/main/java/com/atar/fastlane/MainActivity.java
@@ -3,6 +3,7 @@ package com.atar.fastlane;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.textView1);
         tv.setText("Build Type : "+BuildConfig.BUILD_TYPE +"\nFlavour : "+BuildConfig.FLAVOR);
+        Log.d("atar", "MainActivity... onCreate()...");
+        System.out.println("atar MainActivity... onCreate()...");
     }
 }
